f6c0670939f3b34c7e779bc3f2db2d19cfc52101
ScuderiaFerrari <ipa23.r.pluzmo@vtdt.edu.lv>
2025-04-14 16:54:42 +0300

    neaiztiec skatus kkas sačakarējās bet neaiztiez tikuntā >;(

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,11 +1,10 @@
-<header>
+<header> 
 <nav>
     <ul>
         <li><a href="/">Sākums</a></li>
-        <li><a href="/blogs">Visi blogi</a></li>
-        <li><a href="/categories">Visas kategorijas</a></li>
-        <li><a href="/blogs/create">Veidot blogu</a></li>
-        <li><a href="/categories/create">Veidot kategoriju.</a></li>
+        <li><a href="/register">Reģistrēt</a></li>
+        <li><a href="/login">Ierakstīties</a></li>
     </ul>
 </nav>
 </header>
+<!--te ir navbars oke? :D
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,19 @@
-<p>qeqwe</p>
+<x-layout>
+    <x-slot:title>
+        {{ "Sākumlapa" }}
+    </x-slot:title>
+	<a href="register">Reģistrēties</a>
+	@auth
+		<p>Sveiks, {{ Auth::user()->first_name}}</p>
+		<form action="/logout" method="POST">
+				@csrf
+				<button type="submit">Izrakstīties</button>
+		</form>
+	@endauth
+
+	@guest
+		<p>Sveiks, viesi!</p>
+		<a href="/login">ierkastīties</a>
+	@endguest
+</x-layout>
+balls
